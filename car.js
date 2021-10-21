e21f6f90036aef838acbabb54f68533c3bf1d59d
becca <rebecca.kaci@gmail.com>
2021-10-21 15:32:05 +0200

    Exercice 1 terminé

--- car.js
+++ car.js
@@ -1,3 +1,36 @@
-class car {
-    constructor (brand, speed)
+// Créer un class Car avec 2 propriétés de base:
+
+class Car {
+
+    constructor (brand, speed){
+        this.brand = brand
+        this.speed = speed
+    }
+
+
+    // Ajouter 3 méthodes à la classe Car:
+
+    accelerate(numUp){
+        this.speed = this.speed + numUp
+    }
+
+    brake(numDown){
+        this.speed = this.speed - numDown
+    }
+
+    describe(string){
+        console.log(`${this.brand} running at ${this.speed} km/h`)
+    }
+    
 }
+
+let ford = new Car ("trottinette", 0)
+    ford.accelerate(50)
+    ford.brake(25)
+    console.log(ford);
+
+let elonMusk = new Car ("tesla", 0)
+    elonMusk.accelerate(190)
+    elonMusk.brake(40)
+    console.log(elonMusk);
+
